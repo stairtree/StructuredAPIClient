b982976fb2bb73b8c201815c531017c597302211
Thomas Krajacic <thomas@krajacic.at>
2020-01-03 16:38:13 +0100

    Add logging to TokenAuth

--- Sources/NetworkClient/Transport/TokenAuth.swift
+++ Sources/NetworkClient/Transport/TokenAuth.swift
@@ -1,24 +1,31 @@
 // Copyright © 2020 Stairtree GmbH. All rights reserved.
 
 import Foundation
+import Logging
 
 // Handle token auth and add headers to an existing transport
 public final class TokenAuth: Transport {
     private let base: Transport
-    var auth: AuthState
+    private let logger: Logger
 
-    public init(base: Transport, tokenProvider: TokenProvider) {
+    let auth: AuthState
+
+    public init(base: Transport, tokenProvider: TokenProvider, logger: Logger = Logger(label: "TokenAuth")) {
         self.base = base
-        self.auth = AuthState(provider: tokenProvider)
+        self.logger = logger
+        self.auth = AuthState(provider: tokenProvider, logger: logger)
     }
 
     public func send(request: URLRequest, completion: @escaping (Response) -> Void) {
         self.auth.token { result in
             switch result {
-            case let .failure(error): completion(.error(error))
+            case let .failure(error):
+                completion(.error(error))
             case let .success(token):
+                self.logger.trace("Bearer Token: \(token)")
                 let headers = ["Authorization": "Bearer \(token)"]
-                return AddHeaders(base: self.base, headers: headers).send(request: request, completion: completion)
+                let transport = AddHeaders(base: self.base, headers: headers)
+                transport.send(request: request, completion: completion)
             }
         }
     }
@@ -42,15 +49,19 @@ final class AuthState {
     var refreshToken: Token? = nil
 
     let provider: TokenProvider
+    let logger: Logger
 
-    internal init(provider: TokenProvider) {
+    internal init(provider: TokenProvider, logger: Logger) {
         self.provider = provider
+        self.logger = logger
     }
 
     func token(_ completion: @escaping (Result<String, Error>) -> Void) {
         if let access = self.accessToken, (access.expiresAt ?? Date()) < Date() {
+            logger.trace("Valid access token present")
             return completion(.success(access.raw))
         } else if let refresh = self.refreshToken, (refresh.expiresAt ?? Date()) < Date() {
+            logger.trace("Refreshing token using \(refresh.raw)")
             self.provider.refreshToken(withRefreshToken: refresh, completion: { result in
                 switch result {
                 case let .failure(error):
@@ -61,6 +72,7 @@ final class AuthState {
                 }
             })
         } else {
+            logger.trace("Fetching initial tokens")
             self.provider.fetchToken(completion: { result in
                 switch result {
                 case let .failure(error):
