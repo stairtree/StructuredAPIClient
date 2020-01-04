05bc773c463783afce01a642ac79a9963f8c9576
Thomas Krajacic <thomas@krajacic.at>
2020-01-04 09:40:20 +0100

    Add stacking headers test

--- Tests/NetworkClientTests/NetworkClientTests.swift
+++ Tests/NetworkClientTests/NetworkClientTests.swift
@@ -72,4 +72,41 @@ final class NetworkClientTests: XCTestCase {
 
         XCTAssertEqual(client.baseURL.absoluteString, "https://test.somewhere.com")
     }
+    
+    func testStackingHeaders() {
+        struct TestRequest: NetworkRequest {
+            func makeRequest(baseURL: URL) throws -> URLRequest {
+                return URLRequest(url: baseURL)
+            }
+
+            func parseResponse(_ data: Data) throws -> String {
+                return String(decoding: data, as: UTF8.self)
+            }
+        }
+
+        let responseData = Data("Test".utf8)
+
+        let requestAssertions: (URLRequest) -> Void = {
+            XCTAssertEqual($0.url, URL(string: "https://test.somewhere.com")!)
+            XCTAssertEqual($0.allHTTPHeaderFields?["H1"], "1")
+            XCTAssertEqual($0.allHTTPHeaderFields?["H2"], "2")
+        }
+
+        let base = TestTransport(responseData: [responseData], assertRequest: requestAssertions)
+        let h1 = AddHeaders(base: base, headers: ["H1": "1"])
+        let h2 = AddHeaders(base: h1, headers: ["H2": "2"])
+        
+        let client = NetworkClient(baseURL: URL(string: "https://test.somewhere.com")!, transport: h2)
+
+        client.load(TestRequest()) { result in
+            do {
+                let response = try result.get()
+                XCTAssertEqual(response, "Test")
+            } catch {
+                XCTFail("\(error)")
+            }
+        }
+
+        XCTAssertEqual(client.baseURL.absoluteString, "https://test.somewhere.com")
+    }
 }
