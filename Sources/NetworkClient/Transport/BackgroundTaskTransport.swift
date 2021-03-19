1d8aea1a1d9e8ce3c10172040a326670100b1d99
Thomas Krajacic <thomas@krajacic.at>
2021-03-19 21:17:04 +0100

    First sketch of a BackgroundExtendingTransport
    Cancellation is a bit sketchy

---
+++ Sources/NetworkClient/Transport/BackgroundTaskTransport.swift
@@ -0,0 +1,78 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Network Client open source project
+//
+// Copyright (c) Stairtree GmbH
+// Licensed under the MIT license
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+import Logging
+
+#if !os(macOS)
+import UIKit
+
+public final class BackgroundExtendingTransport: Transport {
+    
+    /// The base `Transport` to extend
+    private let base: Transport
+    
+    /// A debug name for the background task. It will be suffixed with the request's url
+    private let name: String?
+    
+    /// Called synchronously on the main thread shortly before the app is suspended
+    private let expirationHandler: (() -> Void)?
+    
+    private var started: Bool = false
+    
+    public init(base: Transport, name: String?, expirationHandler: (() -> Void)?) {
+        self.base = base
+        self.name = name
+        self.expirationHandler = expirationHandler
+    }
+    
+    public func send(request: URLRequest, completion: @escaping (Response) -> Void) {
+        if #available(
+            iOSApplicationExtension 9,
+            tvOSApplicationExtension 9,
+            macCatalystApplicationExtension 13,
+            watchOS 2,
+            iOS 999, tvOS 999, macCatalyst 999, *
+        ) {
+            let reason = request.debugString
+            ProcessInfo().performExpiringActivity(withReason: reason, using: { expired in
+                // Being called with `expired` without being `started` means
+                // the background assertion was not granted.
+                if expired && !self.started { self.cancel(); return completion(.error(.cancelled)) }
+                self.started = true
+                guard !expired else { return self.cancel() }
+                self.base.send(request: request, completion: completion)
+            })
+        } else {
+            #if !os(watchOS)
+            let reason = "\(name.map { "\($0)-" } ?? "")\(request.debugString)"
+            var identifier: UIBackgroundTaskIdentifier!
+            identifier = UIApplication.shared.beginBackgroundTask(withName: reason, expirationHandler: { [weak self] in
+                self?.expirationHandler?()
+                UIApplication.shared.endBackgroundTask(identifier)
+            })
+            guard identifier != .invalid else { self.cancel(); return completion(.error(.cancelled)) }
+            base.send(request: request) { response in
+                completion(response)
+                UIApplication.shared.endBackgroundTask(identifier)
+            }
+            #endif
+        }
+    }
+    
+    public var next: Transport? { base }
+}
+#endif
