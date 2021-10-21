bfa3b29b383883f217195fcd10ff2398ac65ddb4
Thomas Krajacic <thomas@krajacic.at>
2021-10-21 21:21:35 +0100

    Add an async/await shim for URLSessionTransport

--- Sources/StructuredAPIClient/Transport/URLSessionTransport.swift
+++ Sources/StructuredAPIClient/Transport/URLSessionTransport.swift
@@ -61,6 +61,33 @@ public final class URLSessionTransport: Transport {
     }
 }
 
+@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+extension URLSessionTransport {
+    
+    /// Sends the request using a `URLSessionDataTask`
+    /// - Parameter request: The configured request to send.
+    /// - Returns: The received response from the server.
+    public func send(request: URLRequest) async throws -> TransportResponse {
+        do {
+            let (data, response) = try await session.data(for: request)
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw TransportFailure.network(URLError(.unsupportedURL))
+            }
+            return httpResponse.asTransportResponse(withData: data)
+            
+        } catch let netError as URLError {
+            if netError.code == .cancelled { throw TransportFailure.cancelled }
+            throw TransportFailure.network(netError)
+            
+        } catch let error as TransportFailure {
+            throw error
+            
+        } catch {
+            throw TransportFailure.unknown(error)
+        }
+    }
+}
+
 #if canImport(Combine)
 import Combine
 
